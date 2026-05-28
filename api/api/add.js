9555e95d7957560e8ea4acef6cbdc77381bb4bbd
Zach Upstone <zu213@bath.ac.uk>
2026-05-28 19:30:27 +0100

    Added super simple backend for vercel test

---
+++ api/api/add.js
@@ -0,0 +1,28 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed",
+    });
+  }
+
+  const { value } = req.body;
+
+  if (!value) {
+    return res.status(400).json({
+      error: "Missing value",
+    });
+  }
+
+  await redis.rpush("strings", value);
+
+  res.json({
+    success: true,
+  });
+}
---
+++ api/api/list.js
@@ -0,0 +1,12 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  const strings = await redis.lrange("strings", 0, -1);
+
+  res.json(strings);
+}
